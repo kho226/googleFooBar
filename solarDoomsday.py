6d0b77061a001631bc7badce1cf8a1604d6a705b
Kyle Ong <kho226@nyu.edu>
2017-11-06 12:09:04 -0500

    added solarDoomsday.py

---
+++ solarDoomsday.py
@@ -0,0 +1,73 @@
+'''
+
+Solar Doomsday
+
+Author: Kyle Ong
+Date: 11/05/2017
+
+Problem: Write a function answer(area) that represents the total area of solar panels and returns an arrayList[Int] of ints representing the maximum squares
+Constraints:
+- 0 < area < 1000000
+
+type: area: Int
+rtype : ret: arrayList[Int]
+
+example:
+area = 12
+ret = [9,1,1,1]
+
+
+'''
+
+try:
+	import math
+	import sys
+except ImportError:
+	raise ValueError("Could not import Math")
+
+def answer(area):
+	'''
+
+		Author: Kyle Ong
+		Date: 11/05/2017
+
+		type: area: Int
+		rtype: ret: arrayList[Int]
+
+		type: area: int
+		rtype: ret: arrayList[int]
+
+	'''
+	ret = []
+	if not isValidInput(area):
+		raise ValueError("Invalid Input")
+	maxArea = 0
+	while area > 0:
+		maxArea = int(math.sqrt(area))
+		area = area - maxArea*maxArea
+		ret.append(maxArea*maxArea)
+	print ret
+	return ret
+
+def isValidInput(area):
+	'''
+		Author: Kyle Ong
+		Date: 11/05/2017
+
+		Validates the input
+
+		type: area: int
+		rtype: Bool
+	'''
+	if not (0 < area and area < 1000000) or not area: return False
+	return True
+
+try:
+	userInput = sys.argv[1]
+	answer(int(userInput))
+except:
+	print "Invalid Usage %s. integer!!" %sys.argv[0]
+
+
+
+
